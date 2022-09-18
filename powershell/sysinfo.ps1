f8b91a255cc20a3780424333b1d72aed45d5332e
montgos6 <montgos6@mail.uc.edu>
2022-09-18 19:24:12 -0400

    powershell files

--- powershell/sysinfo.ps1
+++ powershell/sysinfo.ps1
@@ -1,6 +1,22 @@
 ﻿function getIP {
 (get-netipaddress).ipv4address | Select-String "192*"
 }
+function getUser{
+$env:USERNAME
+}
+function getHost{
+$env:COMPUTERNAME
+}
+function getDate {
+get-date
+}
+function getVersion{
+$PSVersionTable.PSVersion
+}
 write-host(getIP)
 $IP = getIP
-Write-Host(This Machine's IP address is $IP")
+$Hostname = getHost
+$User = getUser
+$Version = getVersion
+$DATE = getDate
+Write-Host("This Machines IP address is $IP. User is $User. Hostname is $Hostname. The current powershell shell version is $Version. Today's Date is $DATE")
