6899dd61e69b3055b9e9f1f28d1e5197b74af0a7
montgos6 <64667550+montgos6@users.noreply.github.com>
2022-10-30 17:28:33 -0400

    Add files via upload

---
+++ labs/lab8.py
@@ -0,0 +1,5 @@
+from bs4 import BeautifulSoup
+import requests, re
+r = requests.get('https://www.catalog.update.microsoft.com/Search.aspx?q=2022-10%20windows%2011%2022h2%20x64').content
+soup = BeautifulSoup(r, 'html.parser')
+for row in soup.find_all('a',{"class":"contentTextItemSpacerNoBreakLink"}):print(row.string
