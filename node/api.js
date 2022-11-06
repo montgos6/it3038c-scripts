30083d7c0c0740ac73d33ce3e1922980603736c9
montgos6 <64667550+montgos6@users.noreply.github.com>
2022-11-06 13:43:37 -0500

    Update api.js

--- node/api.js
+++ node/api.js
@@ -13,8 +13,6 @@ const server = http.createServer((req, res) => {
     }
 });
 
-server.listen(3000);
-console.log("Server is listening on port 3000");
 
 const listBlue = (res) => {
   const colorBlue = data.filter((item) => {
@@ -23,3 +21,7 @@ const listBlue = (res) => {
 
   res.end(JSON.stringify(colorBlue));
 }
+
+
+server.listen(3000);
+console.log("Server is listening on port 3000");
